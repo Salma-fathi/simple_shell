b3bd1689b59401dec02c3cde609aff8123c4ba3d
AmnaHussien <amnahussin88@gmail.com>
2023-11-07 01:28:20 -0800

    amna

---
+++ execute_cmd.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * execute_command - function that execute the commands
+ * Description: c programm
+ * @command: argv
+ * Return: the executed command
+ */
+void execute_command(char **argv)
+{
+char *command = 0, *loc_of_command = 0;
+
+if (argv)
+	{
+        
+         command = argv[0];
+         loc_of_command = get_location(command);
+ 
+if (execve(loc_of_command, argv, NULL) == -1)
+	{
+            perror("ERROR"); }
+    }
+
+}
