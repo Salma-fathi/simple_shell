f585155dd8b6b7bafb04bdba6e0e28bcde9ab2cc
Salma-fathi <67196308+Salma-fathi@users.noreply.github.com>
2023-11-13 17:13:21 +0200

    isbuilt

--- handl_builtin.c
+++ handl_builtin.c
@@ -1,5 +1,18 @@
 #include "shell.h"
 
+int is_builtin(char *command)
+{
+	char *builtins[] = {"exit", "env","setevn" "cd", NULL};
+	int i;
+
+	for (i = 0; builtins[i]; i++)
+	{
+		if (_strcmp(command, builtins[i]) == 0)
+			return (1);
+	}
+
+	return (0);
+}
 void handle_built(int *status, char **command, int ind, char **argv)
 {
     (void)argv;
