1a4c0caba99c338d66d88d5de784cfe38bbfd173
Salma-fathi <67196308+Salma-fathi@users.noreply.github.com>
2023-11-15 17:30:56 +0200

    strvar

--- getenvro.c
+++ getenvro.c
@@ -1,50 +1,19 @@
 #include "shell.h"
-
 /**
- * _getenv - function that gets the environment variable value
- * @var: variable name
- * Return: the value of the environment variable, or NULL if not found
+ * _getenv - gets environment path
+ * @var: the env variable
+ * Return: the path
  */
 char *_getenv(char *var)
 {
-    if (var == NULL)
-        return NULL;
+	size_t len = _strlen(var);
+	char **env = environ;
 
-    char *tmp;
-	char *value;
-	char *key;
-	char *env;
-    int i;
-
-    for (i = 0; environ[i] != NULL; i++)
-    {
-        tmp = _strdup(environ[i]);
-        if (tmp == NULL)
-        {
-        
-            return NULL;
-        }
-
-        key = _strtok(tmp, "=");
-        if (key != NULL && _strcmp(var, key) == 0)
-        {
-            value = _strtok(NULL, "\n");
-            env = _strdup(value);
-            free(tmp);
-
-            if (env == NULL)
-            {
-                
-                return NULL;
-            }
-
-            return env;
-        }
-
-        free(tmp);
-        tmp = NULL;
-    }
-
-    return NULL;
+	while (*env)
+	{
+		if (_strncmp(*env, var, len) == 0 && (*env)[len] == '=')
+			return (&(*env)[len + 1]);
+		env++;
+	}
+	return (NULL);
 }
-
