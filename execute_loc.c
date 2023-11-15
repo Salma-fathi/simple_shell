fbff6d24acfd4c6a5529931e41979276656005e7
Salma-fathi <67196308+Salma-fathi@users.noreply.github.com>
2023-11-15 17:03:26 +0200

    hh so fun

--- execute_loc.c
+++ execute_loc.c
@@ -2,7 +2,7 @@
 
 /**
  * exec - executes the user command
- * @argv: the array of the splited command
+ * @arg: the array of the splited command
  * @line: the input of the user
  * @name: shell name
  * @ind: index
@@ -40,41 +40,4 @@ int _execute(char **command, char **argv, int ind)
         free(full_cmnd);
     }
     return WEXITSTATUS(status);
-}
-#include "shell.h"
-
-/**
- * _execute - executes the user command
- * @command: the array of the splited command
- * @argv: the name of the program
- * @ind: index
- */
-void _execute(char **command, char **argv, int ind)
-{
-    pid_t child;
-    int status;
-
-    char full_cmnd[1024]; // Assuming the maximum length of a command path is 1024
-
-    if (!get_path(command[0], full_cmnd))
-    {
-        printerror(argv[0], command[0], ind);
-        freearystring(command);
-        return;
-    }
-
-    child = fork();
-    if (child == 0)
-    {
-        if (execve(full_cmnd, command, environ) == -1)
-        {
-            fprintf(stderr, "%s: %s: %s\n", argv[0], command[0], strerror(errno));
-            exit(EXIT_FAILURE);
-        }
-    }
-    else
-    {
-        waitpid(child, &status, 0);
-        freearystring(command);
-    }
 }
