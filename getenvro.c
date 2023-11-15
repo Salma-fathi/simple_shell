0ac4cf078b8a5ec77e296604ceb343a5efe9fc5a
Salma-fathi <67196308+Salma-fathi@users.noreply.github.com>
2023-11-15 23:26:41 +0200

    shell testss

--- getenvro.c
+++ getenvro.c
@@ -1,36 +1,43 @@
 #include "shell.h"
 
 /**
- * _getenv - function that gets the environment variable value
- * @var: variable name
- * Return: the value of the environment variable, or NULL if not found
- */
+  * _getenv - function that gets the environment variable value
+  * @var: variable name
+  * Return: the value of the environment variable, or NULL if not found
+  */
 char *_getenv(char *var)
-{	char *tmp, *value, *key, *env;
-	int i;
-if (var == NULL)
-	{ return (NULL); }
-
-for (i = 0; environ[i] != NULL; i++)
 {
-	tmp = _strdup(environ[i]);
-	if (tmp == NULL)
-		{ return (NULL); }
-	key = _strtok(tmp, "=");
-	if (key != NULL && _strcmp(var, key) == 0)
-	{
-		value = _strtok(NULL, "\n");
-		env = _strdup(value);
-		free(tmp);
+    if (var == NULL)
+        return NULL;
 
-		if (env == NULL)
-		{ return (NULL); }
-		return (env);
-		free(tmp);
-		tmp = (NULL);
-	}}
-	
-	return (NULL);
+    char *tmp, *value,*keey, *env;
+    int i;
+    for (i = 0; environ[i] != NULL; i++)
+    {
+        tmp = _strdup(environ[i]);
+        if (tmp == NULL)
+        {
+            return NULL;
+        }
 
+        keey = _strtok(tmp, "=");
+        if (keey != NULL && _strcmp(var, keey) == 0)
+        {
+            value = _strtok(NULL, "\n");
+            env = _strdup(value);
+            free(tmp);
 
+            if (env == NULL)
+            {
+                return NULL;
+            }
+
+            return env;
+        }
+
+        free(tmp);
+        tmp = NULL;
+    }
+
+    return NULL;
 }
