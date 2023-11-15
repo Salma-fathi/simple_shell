ff0b91b0571b16959b2c3391d1558af135f83f8d
Salma-fathi <67196308+Salma-fathi@users.noreply.github.com>
2023-11-15 17:00:09 +0200

    cute

--- execute_loc.c
+++ execute_loc.c
@@ -1,6 +1,49 @@
 #include "shell.h"
 
 /**
+ * exec - executes the user command
+ * @argv: the array of the splited command
+ * @line: the input of the user
+ * @name: shell name
+ * @ind: index
+ * Return: the status of the operation
+ */
+
+int _execute(char **command, char **argv, int ind)
+{
+    pid_t child;
+    int status;
+    char *full_cmnd;
+
+    full_cmnd = get_path(command[0]);
+    if (!full_cmnd)
+    {
+        printerror(argv[0], command[0], ind);
+        freearystring(command);
+        return 127;
+    }
+
+    child = fork();
+    if (child == 0)
+    {
+        if (execve(full_cmnd, command, environ) == -1)
+        {
+            perror("execve failed");
+            exit(EXIT_FAILURE);
+        }
+        exit(EXIT_SUCCESS);
+    }
+    else
+    {
+        waitpid(child, &status, 0);
+        freearystring(command);
+        free(full_cmnd);
+    }
+    return WEXITSTATUS(status);
+}
+#include "shell.h"
+
+/**
  * _execute - executes the user command
  * @command: the array of the splited command
  * @argv: the name of the program
