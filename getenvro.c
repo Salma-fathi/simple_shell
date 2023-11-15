d05013cfd4f6c55acb60dd22909ddf81c4ba1052
Salma-fathi <67196308+Salma-fathi@users.noreply.github.com>
2023-11-15 20:12:35 +0200

    tmpnull

--- getenvro.c
+++ getenvro.c
@@ -12,10 +12,10 @@ char *_getenv(char *var)
     if (var == NULL)
         return NULL;
 
-    char *tmp = NULL;
-    char *value = NULL;
-    char *key = NULL;
-    char *env = NULL;
+    char *tmp;
+    char *value;
+    char *key;
+    char *env ;
     int i;
     for (i = 0; environ[i] != NULL; i++)
     {
