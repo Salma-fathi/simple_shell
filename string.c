628c8b3f2429ee9ae9d489acbe29bcd3c24c242f
Salma-fathi <67196308+Salma-fathi@users.noreply.github.com>
2023-11-16 02:25:23 +0200

    loops

--- string.c
+++ string.c
@@ -91,13 +91,14 @@ return (ret);
  */
 int _strcmp(char *s1, char *s2)
 {
-        int i = 0;
-        while (s1[i] - s2[i] == 0 && s1[i] != '\0')
-        {
-                i++;
-        }
-        return (s1[i] - s2[i]);
+    int i = 0;
+    while (s1[i] != '\0' && s1[i] == s2[i])
+    {
+        i++;
+    }
+    return s1[i] - s2[i];
 }
+
 /**
  * _strchr - Returns a pointer to the first
  *       occurrence of the character
