48f069ba21114ac183f4933c9159691d402c90c4
AmnaHussien <amnahussin88@gmail.com>
2023-11-17 00:44:03 -0800

    d

--- get_path.c
+++ get_path.c
@@ -1,31 +1,30 @@
 #include "shell.h"
 /**
- * get_full_path - gets the path of the user command
- * @command: the user input
+ * get_full_path - function that gets the path of the user command
+ * @cmd: the user input
  * @path: the environment path
- * Return: the path
+ * Return: on success the path and on failure 0
  */
-char *get_full_path(char *command, char *path)
+char *get_full_path(char *cmd, char *path)
 {
-	char *token;
-	char *full_path = NULL;
+char *token, *full_path = NULL;
+tokn = _strtok(path, ":");
 
-	token = _strtok(path, ":");
-	while (token != NULL)
+	while (tokn != NULL)
 	{
-		full_path = malloc(_strlen(token) + _strlen(command) + 1);
+		full_path = malloc(_strlen(tokn) + _strlen(cmd) + 1);
 		if (full_path == NULL)
-			return (NULL);
+			{ return (NULL); }
 
-		_strcpy(full_path, token);
+		_strcpy(full_path, tokn);
 		_strcat(full_path, "/");
-		_strcat(full_path, command);
+		_strcat(full_path, cmd);
 
 		if (access(full_path, X_OK) == 0)
-			return (full_path);
+			{ return (full_path); }
 
 		free(full_path);
-		token = _strtok(NULL, ":");
+		tokn = _strtok(NULL, ":");
 	}
 
 	return (NULL);
