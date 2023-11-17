a63b7b0dc8149f1d55209ac2450bf6a360b7c122
AmnaHussien <amnahussin88@gmail.com>
2023-11-17 04:04:03 -0800

    d

--- shell.c
+++ shell.c
@@ -5,7 +5,7 @@
  * Description: c programm
  * @ac: counts arg number
  * @argv: array of string
- * Return: 0 in success and -1 in fail
+ * Return: on success  0 and -1 on failure
  */
 int main(int ac, char **argv)
 {
@@ -14,41 +14,42 @@ int status = 0, index = 0, j;
 (void) ac;
 
 
-	while (1)
+while (1)
+{
+	lin = read_line();
+
+
+	if (lin == NULL)
+		return (status);
+	_comment(lin);
+	if (_isempty(lin))
 	{
-		lin = read_line();
+		free(lin), status = 0;
+		continue;
+	}
+	index++;
+	toknizer = spilt_line(lin, ";");
 
-		if (lin == NULL)
-			return (status);
-		_comment(lin);
-		if (_isempty(lin))
+	if (toknizer == NULL)
+		continue;
+	for (j = 0; toknizer[j] != NULL; j++)
+	{
+		tokns = spilt_line(toknizer[j], " \n\t");
+		if (tokns == NULL)
 		{
-			free(lin), status = 0;
-			continue;
-		}
-		index++;
-		toknizer = spilt_line(lin, ";");
-
-		if (toknizer == NULL)
-			continue;
-		for (j = 0; toknizer[j] != NULL; j++)
-		{
-			tokns = spilt_line(toknizer[j], " \n\t");
-			if (tokns == NULL)
-			{
-				free(tokns);
-				continue;
-			}
-			if (check_built(lin))
-			{
-				handle_built(tokns, status, lin, index, argv, toknizer);
-				continue;
-			}
-			status = _exec(tokns, lin, index, argv);
 			free(tokns);
+			continue;
 		}
-		free(toknizer), free(lin);
+		if (check_built(lin))
+		{
+			handle_built(tokns, status, lin, index, argv, toknizer);
+			continue;
+		}
+		status = _exec(tokns, lin, index, argv);
+		free(tokns);
+	}
+	free(toknizer), free(lin);
 	}
 
-	return (status);
+return (status);
 }
